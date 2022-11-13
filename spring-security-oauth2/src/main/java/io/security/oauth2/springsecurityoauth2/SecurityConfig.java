0cddda54cf2cac195d7f83dfab60460dc077a476
SeoYangHun <syh8088@docfriends.com>
2022-11-13 21:44:16 +0900

    spring security 프로젝트 생성

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -3,18 +3,88 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+//        extends WebSecurityConfigurerAdapter
+{
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        //http.formLogin();
+//        http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("custom entryPoint");
+            }
+        });
         return http.build();
     }
+
+//    @Bean
+//    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated();
+//        http.formLogin();
+//        //http.apply(new CustomSecurityConfigurer().setFlag(false));
+//        return http.build();
+//    }
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated();
+//        http.httpBasic();
+//        return http.build();
+//    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
