dd847208995c580e73c552fc76bca059b42048dc
SeoYangHun <syh8088@docfriends.com>
2022-11-13 21:49:14 +0900

    폴더명 변경

--- spring-security-oauth2-test/src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ spring-security-oauth2-test/src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityOauth2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2Application.class, args);
+    }
 
 }
